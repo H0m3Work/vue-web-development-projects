99678de2bee9e60673a680114e08da1519d68205
Vu Hung Cuong <cuongvh@gemvietnam.com>
2020-06-18 14:48:20 +0700

    Done Vuejs

--- markdown_notebook/script.js
+++ markdown_notebook/script.js
@@ -1,3 +1,5 @@
+Vue.filter('date', time => moment(time).format('DD/MM/YY, HH:mm'))
+
 // New vuejs instance
 new Vue({
 	// CSS selector of the root DOM element
@@ -10,7 +12,7 @@ new Vue({
 			// content: 'This is a note',
 			notes: JSON.parse(localStorage.getItem('notes')) || [],
 			// Id of the selected note
-			selectedId: null,
+			selectedId: localStorage.getItem('selected-id') || null,
 		}
 	},
 
@@ -25,6 +27,36 @@ new Vue({
 		selectedNote () {
 			// We return the matching note with selectedId
 			return this.notes.find(note => note.id === this.selectedId)
+		},
+		sortedNotes () {
+			return this.notes.slice()
+				.sort((a, b) => a.created - b.created)
+				.sort((a, b) => (a.favorite === b.favorite) ? 0 
+					: a.favorite ? -1 : 1)
+		},
+		linesCount () {
+			if (this.selectedNote) {
+				// Count the number of new line characters
+				return this.selectedNote.content.split(/\r\n|\r|\n/).length
+			}
+		},
+		wordsCount () {
+			if (this.selectedNote) {
+				var s = this.selectedNote.content
+				// Turn new line characters into white-spaces
+				s = s.replace(/\n/g, ' ')
+				// Exclude start and end white-spaces
+				s = s.replace(/(^\s*)|(\s*$)/gi, '')
+				// Turn 2 or more duplicate white-spaces into 1
+				s = s.replace(/\s\s+/gi, ' ')
+				// Return the number of spaces
+				return s.split(' ').length
+			}
+		},
+		charactersCount () {
+			if (this.selectedNote) {
+				return this.selectedNote.content.split('').length
+			}
 		}
 	},
 
@@ -35,6 +67,10 @@ new Vue({
 			handler: 'saveNote',
 			// We need this to watch each note's properties inside the array
 			deep: true,
+		},
+		// Let's save the selection too
+		selectedId (val) {
+			localStorage.setItem('selected-id', val)
 		}
 	},
 
@@ -65,7 +101,20 @@ new Vue({
 		selectNote (note) {
 			this.selectedId = note.id
 			console.log(this.selectedId)
+		},
+		removeNote () {
+			if (this.selectedNote && confirm('Delete the note?')) {
+				// Remove the note in the notes array
+				const index = this.notes.indexOf(this.selectedNote)
+				if (index !== -1) {
+					this.notes.splice(index, 1)
+				}
+			}
+		},
+		favoriteNote () {
+			this.selectedNote.favorite ^= true
 		}
+
 	},
 
 	// This will be called when the instance is ready
